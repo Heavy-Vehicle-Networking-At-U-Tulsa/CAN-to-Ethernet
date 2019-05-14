8478308b37882413097abaa04d625aa321337a5e
benettlinger <39038496+benettlinger@users.noreply.github.com>
2019-05-14 09:32:28 -0500

    Add files via upload

--- examples/Test_CAN_Tx.ino
+++ examples/Test_CAN_Tx.ino
@@ -2,17 +2,10 @@
 #include <SPI.h>
 #include <FlexCAN.h>
 //Create a counter to keep track of message traffic
-uint32_t RXCount0 = 0;
-uint32_t RXCount1 = 0;
 uint32_t TXCount = 0;
 
-
-//Define CAN Message Type
-static CAN_message_t rxmsg0;
-static CAN_message_t rxmsg1;
+//Define CAN message data type
 static CAN_message_t txmsg;
-
-//Define LED boolean
 boolean LED_state;
 
 //A generic CAN Frame print function for the Serial terminal
@@ -29,8 +22,6 @@ void printFrame(CAN_message_t rxmsg, uint8_t channel, uint32_t RXCount)
   Serial.println();
 }
 
-//Setup block
-//Code that only runs once
 void setup()
 {
   pinMode(14, OUTPUT);
@@ -49,19 +40,29 @@ void setup()
   LED_state = true;
   digitalWrite(LED_BUILTIN, LED_state);
   Serial.println("Starting CAN test.");
+  txmsg.id = 0xABCDEF00;
+  txmsg.ext = 1;
+  txmsg.len = 8;
+  txmsg.buf[0] = 0x11;
+  txmsg.buf[1] = 0x22;
+  txmsg.buf[2] = 0x33;
+  txmsg.buf[3] = 0x44;
+  txmsg.buf[4] = 0x55;
+  txmsg.buf[5] = 0x66;
+  txmsg.buf[6] = 0x77;
+  txmsg.buf[7] = 0x88;
 }
 
-//Loop Block
-void loop()
-{
-  
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(500);
   while (true){
-    if(Can0.read(rxmsg0)){
-      printFrame(rxmsg0, 0, RXCount0++);
+    if (TXCount % 2 == 0){
+      Can0.write(txmsg);
     }
-    if(Can1.read(rxmsg1)){
-      printFrame(rxmsg1, 0, RXCount0++);
+    else{
+      Can1.write(txmsg);
     }
+    TXCount++;
   }
-  
 }
